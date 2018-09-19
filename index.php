1bca84daf5f69703f44d9bc31e9126711824b4da
justsenne <senne1320@gmail.com>
2018-09-19 14:13:22 +0200

    incompetenties verwijderd

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <title>Aanmelden - Jaimyvg</title>
         <link rel="stylesheet" href="stylesheets/styles.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
     </head>
     <body>
         <!-- Nav begin-->
