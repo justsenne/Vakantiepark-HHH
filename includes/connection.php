d5c49fbe08eb800cd7fa377ea0d72f91f0a5f6ef
DylanWilbrink <dylanwilbrink@gmail.com>
2018-09-20 11:40:31 +0200

    register

--- includes/connection.php
+++ includes/connection.php
@@ -1,9 +1,14 @@
 <?php
-try {
-    global $conn;
-    $conn = new PDO('mysql:host=localhost;dbname=hhh', "root", "");
+
+class connection
+{
+    public function connect()
+    {
+        try {
+            global $conn;
+            $conn = new PDO('mysql:host=localhost;dbname=hhh', "root", "");
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
 }
-catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
---
+++ php/register.php
@@ -0,0 +1,30 @@
+<?php
+include('../includes/connection.php');
+$connection = new connection;
+
+if (isset($_POST['submit'])) {
+
+    if ($_POST['email'] && $_POST['password']) {
+        //Insert into DB
+        $query = $connection->connect()->prepare('INSERT INTO users (email, password, first_name, surname, phone_number, street_name, house_number) 
+                                                      VALUES (:email, :password, :first_name, :surname, :phone_number, :street_name, :house_number)');
+
+        $query->execute(array(
+            ':email' => $_POST['email'],
+            ':password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
+            ':first_name' => $_POST['first_name'],
+            ':surname' => $_POST['surname'],
+            ':phone_number' => $_POST['phone_number'],
+            ':street_name' => $_POST['street_name'],
+            ':house_number' => $_POST['house_number']
+        ));
+        echo 'Inserted';
+        header('location: ../index.php');
+        $_SESSION['error'] = '';
+    } else {
+        $_SESSION['error'] = 'Please fill out the entire form.';
+        header('location: register.php');
+    }
+} else {
+    echo 'Nothing was inserted';
+}
