74bcad6b27bfb40b328a846e15cc837603021f97
DylanWilbrink <dylanwilbrink@gmail.com>
2018-09-20 11:42:38 +0200

    no more class

--- includes/connection.php
+++ includes/connection.php
@@ -1,14 +1,7 @@
 <?php
-
-class connection
-{
-    public function connect()
-    {
-        try {
-            global $conn;
-            $conn = new PDO('mysql:host=localhost;dbname=hhh', "root", "");
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
-    }
+try {
+    global $conn;
+    $conn = new PDO('mysql:host=localhost;dbname=hhh', "root", "");
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
