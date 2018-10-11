df92a914e4999e47572b7c0356cbc69809f0c927
jaimyvangerrevink <36194767+jaimyvangerrevink@users.noreply.github.com>
2018-10-11 11:10:50 +0200

    losse nav-foot ofzo fix hoop?

---
+++ includes/footer.php
@@ -0,0 +1,55 @@
+<div class="footer">
+    <div class="content">
+        <div class="container">
+            <ul>
+                <li>
+                    <img class="logo" src="Untitled.png" alt="">
+                </li>
+                <li>
+                    <h5>
+                        Qwerty logo
+                    </h5>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h5>
+                        Klantenservice
+                    </h5>
+                </li>
+                <li>
+                    <a href="#VeelGesteldeVragen">
+                        Veel gestelde vragen
+                    </a>
+                </li>
+                <li>
+                    <a href="#Contact">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h5>
+                        Privacy & Voorwaarden
+                    </h5>
+                </li>
+                <li>
+                    <a href="#AlgemeneVoorwaarden">
+                        ik hou van bukkake
+                    </a>
+                </li>
+                <li>
+                    <a href="#Privacy">
+                        Privacy
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            Copyright © 2018 Happy Home Holidays, Alle rechten voorbehouden.
+        </div>
+    </div>
+</div>
