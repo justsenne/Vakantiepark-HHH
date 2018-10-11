efacaba9d9a63d0bd49ad3b72304765b56cb815a
DylanWilbrink <dylanwilbrink@gmail.com>
2018-10-11 11:42:14 +0200

    jaimy's kankerzooi

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,19 @@
-<div>
-    <h1>
-        dit is een navbar
-    </h1>
+<div class="nav">
+    <div class="container">
+        <img class="logo" src="Untitled.png" alt="">
+        <ul>
+            <li>
+                <a href="#">Vakantieparken</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+            <li>
+                <div class="splitter"></div>
+            </li>
+            <li>
+                <a class="button" href="#">Aanmelden <i class="material-icons">navigate_next</i></a>
+            </li>
+        </ul>
+    </div>
 </div>
--- pages/addhome.php
+++ pages/addhome.php
@@ -3,7 +3,24 @@
 
 </head>
 <body>
+    <form>
+        naam:
+        <input type="text" name="naam"><br/>
+        omschrijving:
+        <input type="text" name="omschrijving"><br/>
+        locatie:
+        <input type="text" name="locatie"><br/>
+        Afwerkingsniveau:
+        <input type="radio" name="gender" value="zilver"> zilver
+        <input type="radio" name="gender" value="goud"> goud<br>
+        pluspunten:
+        <input type="text" name="pluspunten"><button>+</button><br/>
+        minpunten:
+        <input type="text" name="minputen"><button>+</button><br/>
+        Foto´s:
+        <input type="file" name="fotos"><br/>
 
-</form>
+        <input type="submit" name="button" value="Huis toevoegen">
+    </form>
 </body>
 </html>
